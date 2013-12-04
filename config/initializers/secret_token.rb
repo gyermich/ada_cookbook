b6c9dbd4fad48d6d06e4f58d517a72b31ae86989
Galina <yermichevagalina@gmail.com>
2013-12-04 12:48:59 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdaCookbook::Application.config.secret_key_base = '203d5049b66e2d3340d33ec3e52a4ff357b431f4cb5f1c0439f49f6f48e21cc5ae161fb9e2f3320d3f4846f2758d57e8b5e6430c975669e67647545a4110f441'
