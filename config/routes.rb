0b6582cd5f3b09671dddeefabe28fd3449274416
Your Name <blake.jennings@gmail.com>
2013-12-05 10:31:50 -0800

    delete button, gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ AdaCookbook::Application.routes.draw do
 
   resources :users
 
+  get '/cookbooks/remove_recipe_from_cookbook/:id', :controller => 'cookbook', :action => 'remove_recipe_from_cookbook'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
